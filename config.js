ed3aaf6760bff31d9c56cd1c318e87f30f29d967
unknown <zachariah11jerobert@gmail.com>
2022-04-01 17:27:57 +0530

    done changes in schemas

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-module.exports={
-    MONGODB:'mongodb+srv://jerome_merng:7PGHepCkfm5nccLp@cluster0.ieodt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
+module.exports = {
+  // MONGODB:'mongodb+srv://jerome_merng:7PGHepCkfm5nccLp@cluster0.ieodt.mongodb.net/merng?retryWrites=true&w=majority'
+  MONGODB: " mongodb://localhost:27017/merng",
+};
